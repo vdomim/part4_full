828505d3d48e33a2758d3fea78e7755d37cceabc
Victor Dominguez <vdomim@gmail.com>
2023-07-27 01:22:04 +0200

    Uploading exercise 4.4

--- blogslists_backend/utils/list_helper.js
+++ blogslists_backend/utils/list_helper.js
@@ -1,6 +1,9 @@
 // eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => 1
 
+const totalLikes = (blogs) => blogs.reduce((sum, blog) => blog.likes + sum, 0)
+
 module.exports = {
     dummy,
+    totalLikes,
 }
