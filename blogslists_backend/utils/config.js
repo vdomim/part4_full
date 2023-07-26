d669cb3f6d030b0acb029092ead6bd6deec39953
Victor Dominguez <vdomim@gmail.com>
2023-07-27 00:57:12 +0200

    Uploading exercise 4.3

--- blogslists_backend/utils/config.js
+++ blogslists_backend/utils/config.js
@@ -1,6 +1,9 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 require('dotenv').config()
 
+// eslint-disable-next-line prefer-destructuring
 const PORT = process.env.PORT
+// eslint-disable-next-line prefer-destructuring
 const MONGODB_URI = process.env.MONGODB_URI
 
 module.exports = {
--- blogslists_backend/utils/middleware.js
+++ blogslists_backend/utils/middleware.js
@@ -12,12 +12,14 @@ const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
 
+// eslint-disable-next-line consistent-return
 const errorHandler = (error, request, response, next) => {
     logger.error(error.message)
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
+    }
+    if (error.name === 'ValidationError') {
         return response.status(400).json({ error: error.message })
     }
 
