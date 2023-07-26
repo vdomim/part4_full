609b63345f4b6ee4f191adbfbc84b6ba69ca9c4d
Victor Dominguez <vdomim@gmail.com>
2023-07-27 00:10:57 +0200

    Uploading exercise 4.2

---
+++ blogslists_backend/models/blog.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number,
+})
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        // eslint-disable-next-line no-param-reassign, no-underscore-dangle
+        returnedObject.id = returnedObject._id.toString()
+        // eslint-disable-next-line no-underscore-dangle, no-param-reassign
+        delete returnedObject._id
+        // eslint-disable-next-line no-underscore-dangle, no-param-reassign
+        delete returnedObject.__v
+    },
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
