917cc1bec4fd0c76093d44c5c7da5dc1aa3a4045
Victor Dominguez <vdomim@gmail.com>
2023-07-28 00:05:49 +0200

    uploading exercise 4.6

--- blogslists_backend/utils/list_helper.js
+++ blogslists_backend/utils/list_helper.js
@@ -17,8 +17,37 @@ const favoriteBlog = (blogs) =>
             return acc
         })
 
+const mostBlogs = (blogs) => {
+    // eslint-disable-next-line no-return-assign, no-param-reassign
+    const blogCountsByAuthor = {}
+
+    blogs.forEach((blog) => {
+        // eslint-disable-next-line operator-linebreak
+        blogCountsByAuthor[blog.author] =
+            (blogCountsByAuthor[blog.author] ?? 0) + 1
+    })
+
+    const entries = Object.entries(blogCountsByAuthor)
+
+    const result = entries.reduce(
+        (max, current) => {
+            if (current[1] > max[1]) {
+                return current
+            }
+            return max
+        },
+        // eslint-disable-next-line comma-dangle
+        ['', 0]
+    )
+    return {
+        author: result[0],
+        blogs: result[1],
+    }
+}
+
 module.exports = {
     dummy,
     totalLikes,
     favoriteBlog,
+    mostBlogs,
 }
