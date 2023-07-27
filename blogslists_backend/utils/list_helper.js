62b8f43f5eb1b9c12041e1af3835cd507b492b07
Victor Dominguez <vdomim@gmail.com>
2023-07-27 14:31:00 +0200

    Uploading exercise 4.5

--- blogslists_backend/utils/list_helper.js
+++ blogslists_backend/utils/list_helper.js
@@ -3,7 +3,22 @@ const dummy = (blogs) => 1
 
 const totalLikes = (blogs) => blogs.reduce((sum, blog) => blog.likes + sum, 0)
 
+const favoriteBlog = (blogs) =>
+    blogs
+        .map((blog) => ({
+            title: blog.title,
+            author: blog.author,
+            likes: blog.likes,
+        }))
+        .reduce((acc, blog) => {
+            if (blog.likes > acc.likes) {
+                return blog
+            }
+            return acc
+        })
+
 module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog,
 }
