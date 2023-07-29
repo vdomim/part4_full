3c7c1323dce3cda2fa201e78b691bc8a2590bbd6
Victor Dominguez <vdomim@gmail.com>
2023-07-29 03:16:36 +0200

    Fix exercise 4.8

--- blogslists_backend/controllers/blogs.js
+++ blogslists_backend/controllers/blogs.js
@@ -1,12 +1,13 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-    Blog.find({}).then((blogs) => {
-        response.json(blogs)
-    })
+// Peticion GET para recuperar todos los blogs de la BD
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
 })
 
+// Peticion POST para insertar un nuevo blog en la BD
 blogsRouter.post('/', (request, response) => {
     const blog = new Blog(request.body)
 
