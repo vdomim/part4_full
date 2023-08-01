a2040eb9c9bc3a6a4d074b68ede5ba71383b7b4a
Victor Dominguez <vdomim@gmail.com>
2023-08-02 00:31:16 +0200

    Uploading exercise 4.18

--- blogslists_backend/app.js
+++ blogslists_backend/app.js
@@ -6,6 +6,7 @@ const config = require('./utils/config')
 require('express-async-errors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 
@@ -29,6 +30,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
---
+++ blogslists_backend/controllers/login.js
@@ -0,0 +1,36 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable consistent-return */
+/* eslint-disable operator-linebreak */
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const loginRouter = require('express').Router()
+const User = require('../models/user')
+
+loginRouter.post('/', async (request, response) => {
+    const { body } = request
+
+    const user = await User.findOne({ username: body.username })
+    const passwordCorrect =
+        user === null
+            ? false
+            : await bcrypt.compare(body.password, user.passwordHash)
+
+    if (!(user && passwordCorrect)) {
+        return response.status(401).json({
+            error: 'invalid username or password',
+        })
+    }
+
+    const userForToken = {
+        username: user.username,
+        id: user.id,
+    }
+
+    const token = jwt.sign(userForToken, process.env.SECRET)
+
+    response
+        .status(200)
+        .send({ token, username: user.username, name: user.name })
+})
+
+module.exports = loginRouter
