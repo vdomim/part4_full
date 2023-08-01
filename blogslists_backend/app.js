6c46bda13a1a411d339d24086c8bfe29c0e78e0d
Victor Dominguez <vdomim@gmail.com>
2023-08-01 20:13:47 +0200

    Uploading exercise 4.15

--- blogslists_backend/app.js
+++ blogslists_backend/app.js
@@ -5,6 +5,7 @@ const config = require('./utils/config')
 // eslint-disable-next-line import/no-extraneous-dependencies
 require('express-async-errors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 
@@ -27,6 +28,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
---
+++ blogslists_backend/controllers/users.js
@@ -0,0 +1,29 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable comma-dangle */
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.get('/', async (request, response) => {
+    const users = await User.find({})
+    response.json(users)
+})
+
+usersRouter.post('/', async (request, response) => {
+    const { body } = request
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+    const user = new User({
+        username: body.username,
+        name: body.name,
+        passwordHash,
+    })
+
+    const savedUser = await user.save()
+
+    response.json(savedUser)
+})
+
+module.exports = usersRouter
